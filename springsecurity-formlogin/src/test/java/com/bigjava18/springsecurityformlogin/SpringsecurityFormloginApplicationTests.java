5da0ed82257afe171cd3425493cb5388e0109261
zgp <2088220905@qq.com>
2021-05-25 09:37:02 +0800

    springsecurity-formlogin

---
+++ springsecurity-formlogin/src/test/java/com/bigjava18/springsecurityformlogin/SpringsecurityFormloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigjava18.springsecurityformlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityFormloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
